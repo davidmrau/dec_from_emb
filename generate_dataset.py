880b7c4871a39ff4edcdacf65a8e652e7438be4f
davidmrau <d.m.rau@uva.nl>
2024-02-29 10:06:55 +0100

    wip

---
+++ generate_dataset.py
@@ -0,0 +1,38 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import Trainer, TrainingArguments
+from torch.utils.data import random_split
+import torch
+import datasets
+import os
+from tqdm import tqdm
+from peft import PeftModel, PeftConfig
+
+
+
+def load_embeddings(input_path):
+    embds = list()
+    for i, emb_file in tqdm(enumerate(os.listdir(input_path)), desc='Loading embeddings...'):
+        if i > 1:
+            break
+        if emb_file.endswith('.pt'):
+            emb = torch.load(f'{input_path}/{emb_file}')
+            embds.append(emb)
+
+    embds = torch.concat(embds)
+    return embds
+
+embeddings_path = '/beegfs/scratch/user/drau/research/reml/indexes/kilt-100w_doc_castorini_repllama-v1-7b-lora-passage'
+dataset_path = '/beegfs/scratch/user/drau/research/reml/datasets/kilt-100w_full/'
+
+model_name = 'castorini/repllama-v1-7b-lora-passage'
+input_embeddings = load_embeddings(embeddings_path).numpy()
+docs_dataset = datasets.load_from_disk(dataset_path).select(range(len(input_embeddings)))
+docs = docs_dataset['content']
+
+# Initialize the model and tokenizer
+
+
+dataset = datasets.Dataset.from_dict({'labels': docs, 'inputs_embeds': input_embeddings})
+
+# Add the new feature column to the dataset
+dataset.save_to_disk('kilt-100w_emb_auto_reg_pretrain_task.hf')
--- poc_decode_emb.py
+++ poc_decode_emb.py
@@ -1,24 +1,25 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, DataCollatorForLanguageModeling
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from transformers import Trainer, TrainingArguments
-from torch.utils.data import random_split
 import torch
 import datasets
-from peft import PeftConfig, LoraConfig, get_peft_model, prepare_model_for_kbit_training
-from torch.utils.data import DataLoader
-import numpy as np
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 import datasets
-from transformers.integrations import WandbCallback
-import warnings
-import random 
+from modeling_llama import LlamaForCausalLM
 
 import os 
-# model_name = 'meta-llama/Llama-2-7b-chat-hf'
+#model_name = 'meta-llama/Llama-2-7b-chat-hf'
+# model_name = 'google/gemma-2b-it'
 model_name = 'TinyLlama/TinyLlama-1.1B-Chat-v1.0'
-tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side='right')
-        # add doc_emb token
-tokenizer.doc_emb_token = '[DOC_EMB]'
-tokenizer.add_tokens([tokenizer.doc_emb_token], special_tokens=False) 
-tokenizer.doc_emb_id = tokenizer.encode(tokenizer.doc_emb_token, add_special_tokens=False)[0]
+tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side='right', torch_dtype=torch.bfloat16)
+ # add tokens
+tokenizer.emb_token = '<emb>'
+tokenizer.add_tokens([tokenizer.emb_token], special_tokens=True) 
+tokenizer.emb_token_id = tokenizer.encode(tokenizer.emb_token, add_special_tokens=False)[0]
+
+tokenizer.agg_token = '<agg>'
+tokenizer.add_tokens([tokenizer.agg_token], special_tokens=True) 
+tokenizer.agg_token_id = tokenizer.encode(tokenizer.agg_token, add_special_tokens=False)[0]
+
 tokenizer.pad_token = tokenizer.bos_token
 
 
@@ -31,24 +32,32 @@ quant_config = BitsAndBytesConfig(
                     bnb_4bit_quant_type='nf4',
                     bnb_4bit_compute_dtype='bfloat16',
                 )
-model = AutoModelForCausalLM.from_pretrained(
+
+
+
+# model = AutoModelForCausalLM.from_pretrained(
+#     model_name, 
+#     quantization_config=quant_config, 
+#     attn_implementation="flash_attention_2",
+#     device_map='auto',
+# )
+
+model = LlamaForCausalLM.from_pretrained(
     model_name, 
     quantization_config=quant_config, 
-    attn_implementation="flash_attention_2",
     device_map='auto',
 )
-
 # model.merge_and_unload()
 model.config.use_cache = False
 model = model.bfloat16()
 model.config.pretraining_tp = 1
 model.resize_token_embeddings(len(tokenizer))
-
 model = prepare_model_for_kbit_training(model)
 
 lora_config = LoraConfig(
     target_modules=['q_proj', 'down_proj', 'gate_proj', 'k_proj', 'v_proj', 'o_proj', 'up_proj'],
     )
+
 print(lora_config)
 # get adapter
 model = get_peft_model(model, lora_config)
@@ -56,10 +65,12 @@ model.print_trainable_parameters()
 doc_max_length = 16
 
 def collate_fn(examples):
-    inp = [tokenizer.doc_emb_token + ' '.join(e['content'].split(' ')[:doc_max_length]) + tokenizer.eos_token for e in examples]
+    inp = [tokenizer.emb_token + ' '.join(e['content'].split(' ')[:doc_max_length]) + tokenizer.agg_token for e in examples]
     inp = tokenizer(inp, return_tensors='pt', padding=True)
-    label = inp['input_ids'].clone()
-    label[label == tokenizer.doc_emb_id] = -100
+    inp_label = [tokenizer.emb_token + ' '.join(e['content'].split(' ')[:doc_max_length]) + tokenizer.eos_token for e in examples]
+    inp_label = tokenizer(inp_label, return_tensors='pt', padding=True)
+    label = inp_label['input_ids']
+    label[label == tokenizer.emb_token_id] = -100
     label[label == tokenizer.bos_token_id] = -100
     label[label == tokenizer.pad_token_id] = -100
     inp.update({'label': label})
@@ -67,9 +78,9 @@ def collate_fn(examples):
 
 os.environ["WANDB_PROJECT"] = 'dec_from_emb'
 
-def get_eos_repr(model, inp):
-    emb = model(**inp, output_hidden_states=True).hidden_states[-1]
-    mask = inp['input_ids'] == tokenizer.eos_token_id
+def get_emb_repr(model, inp):
+    emb = model(**inp, output_hidden_states=True, is_causal=False).hidden_states[-1]
+    mask = inp['input_ids'] == tokenizer.agg_token_id
     return emb[mask].unsqueeze(1)
 
 def replace_embeddings(input_ids, inputs_embeds, insert_embeds, placeholder_id):
@@ -87,34 +98,37 @@ class MyTrainer(Trainer):
         input_ids = inputs['input_ids']
         attention_mask = inputs['attention_mask']
         label = inputs.pop('label')
-        eos_emb = get_eos_repr(model, inputs)
+        emb = get_emb_repr(model, inputs)
         inputs_embeds = model.get_input_embeddings()(input_ids)
-        inputs_embeds = replace_embeddings(input_ids, inputs_embeds, eos_emb, tokenizer.doc_emb_id)
+        inputs_embeds = replace_embeddings(input_ids, inputs_embeds, emb, tokenizer.emb_token_id)
         # inputs_embeds = torch.cat((inputs_embeds[:, 0].unsqueeze(1), eos_emb, inputs_embeds[:, 2:]), 1)
         out = model(inputs_embeds=inputs_embeds, attention_mask=attention_mask, labels=label)
         return out.loss
 
-training_args = TrainingArguments(
-    output_dir="./emb_real",
-    overwrite_output_dir=True,
-    num_train_epochs=10,
-    learning_rate=5e-3,
-    per_device_train_batch_size=64,
-    per_device_eval_batch_size=64,
-    logging_steps=10,  # Log every 1000 steps
-    remove_unused_columns=False,
-    gradient_accumulation_steps=1,
-    report_to=None,
-    save_strategy="steps",
-    save_steps=50,
-    do_eval=True,
-)
-trainer = MyTrainer(
-    model=model,
-    args=training_args,
-    data_collator=collate_fn,
-    train_dataset=dataset,
-)
 
-# Training loop
-trainer.train()
+if __name__ == "__main__": 
+    out = model_name.replace('/', '_')
+    training_args = TrainingArguments(
+        output_dir=f"./emb_{out}",
+        overwrite_output_dir=True,
+        num_train_epochs=10,
+        learning_rate=5e-3,
+        per_device_train_batch_size=128,
+        per_device_eval_batch_size=128,
+        logging_steps=10,  # Log every 1000 steps
+        remove_unused_columns=False,
+        gradient_accumulation_steps=1,
+        report_to=None,
+        save_strategy="steps",
+        save_steps=10,
+        do_eval=True,
+    )
+    trainer = MyTrainer(
+        model=model,
+        args=training_args,
+        data_collator=collate_fn,
+        train_dataset=dataset,
+    )
+    tokenizer.save_pretrained(out)
+    # Training loop
+    trainer.train()
